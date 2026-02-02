39d80b24e49f51ab57a03bdd7df70687e07740c1
unknown <altharasjid@gmail.com>
2026-02-02 17:07:10 +0700

    week01: update CircleCalculator with checkSize logic

---
+++ src/oop_0000136220_altha/week01/CircleCalculator.kt
@@ -0,0 +1,23 @@
+package oop_0000136220_altha.week01
+
+fun main(args: Array<String>) {
+
+    var radius: Double = 7.0
+    var pi: Double = 3.14
+
+    var area: Double = pi * radius * radius
+
+    // Menggunakan string template agar lebih rapi
+    println("Radius: $radius, Area: $area")
+
+    checkSize(area)
+}
+
+fun checkSize(area: Double) {
+    if (area > 100) {
+        println("This is a Big Circle")
+    } else {
+        println("This is a Small Circle")
+        
+    }
+}
