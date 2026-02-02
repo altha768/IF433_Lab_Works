8699befb4288a157bddc04af8d037e1c7c6eaa98
unknown <altharasjid@gmail.com>
2026-02-02 16:50:47 +0700

    week01: add calculateStatus function

--- src/oop_0000136220_altha/week01/GradingSystem.kt
+++ src/oop_0000136220_altha/week01/GradingSystem.kt
@@ -8,11 +8,17 @@ fun main () {
     println("Name: " + name + ", Nilai: " + score)
 
     val grade = when (score) {
-        in 90..100 ->"A"
-        in 80..100 ->"B"
-        in 70..100 ->"C"
-        else ->"D"
+        in 90..100 -> "A"
+        in 80..100 -> "B"
+        in 70..100 -> "C"
+        else -> "D"
 
+    }
+    println(grade)
+
+        fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
+
+                println ("Status: ${calculateStatus(score)}")
 
 
     }
